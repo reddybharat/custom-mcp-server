6efd3bab47404128c9606a1271d67befa99d7c0b
reddybharat <reddy.bharat.11@gmail.com>
2026-04-19 23:56:11 +0530

    Added streamlit app for testing

---
+++ test/config.py
@@ -0,0 +1,10 @@
+server_config = {
+    "Math": {
+        "url": "http://localhost:8000/math/mcp",
+        "transport": "streamable_http",
+    },
+    "Weather": {
+        "url": "http://localhost:8000/weather/mcp",
+        "transport": "streamable_http",
+    },
+}
